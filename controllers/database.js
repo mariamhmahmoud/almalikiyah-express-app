4861bf50617f8c7f0ada608c1e9665e98c8052e5
Mariam Mahmoud <mariamhm@umich.edu>
2023-07-12 20:52:47 -0400

    Restructured app, added routes for login

---
+++ controllers/database.js
@@ -0,0 +1,72 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const pool = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+}).promise();
+
+export async function get_admin_by_username(username) {
+    const [admin] = await pool.query("SELECT * FROM ADMIN WHERE username = ?", [username])
+    return admin;
+}
+
+export async function get_admin_by_refresh_token(refreshToken) {
+    const [admin] = await pool.query("SELECT * FROM ADMIN WHERE refresh_token = ?", [refreshToken])
+    return admin;
+}
+
+export async function update_refresh_token(refreshToken, username) {
+    await pool.query("UPDATE ADMIN SET refresh_token = ? WHERE username = ?", [refreshToken, username])
+
+}
+
+export async function logout(refreshToken) {
+    if (!refreshToken) return res.sendStatus(204);
+    const [admin] = await pool.query("SELECT * FROM ADMIN WHERE refresh_token = ?", [refreshToken])
+    if (!admin[0]) return res.sendStatus(204);
+    const username = admin[0].username;
+    await pool.query("UPDATE ADMIN SET refresh_token = ? WHERE username = ?", [null, username])
+
+    res.clearCookie('refreshToken');
+    return res.sendStatus(200);
+}
+
+export async function register_user(req) {
+    var firstName = req.body["firstName"]
+    var lastName = req.body["lastName"]
+    var email = req.body["email"]
+    var phoneNumber = req.body["phoneNumber"]
+    var address = req.body["address"]
+    var birthday = req.body["birthday"]
+
+    // insert into users table
+    var queryText = 'INSERT INTO users(firstName, lastName, email, phoneNumber, address, birthday) VALUES(?, ?, ?, ?, ?, ?)'
+    var result = await pool.query(queryText, [firstName, lastName, email, phoneNumber, address, birthday]);
+
+    // insert into spouses table
+    spouse = req.body["spouse"]
+    firstName = spouse["firstName"]
+    lastName = spouse["lastName"]
+    birthday = spouse["birthday"]
+    queryText = 'INSERT INTO spouses(userid, firstName, lastName, birthday) VALUES($1, $2, $3, $4)'
+    result = await pool.query(queryText, [parseInt(userid), firstName, lastName, birthday])
+
+    // // insert into childrens table
+    // childrenList = req.body["children"]
+    // for (let i = 0; i < childrenList.length; ++i) {
+    //     firstName = childrenList[i]["firstName"]
+    //     lastName = childrenList[i]["lastName"]
+    //     birthday = childrenList[i]["birthday"]
+
+    //     queryText = 'INSERT INTO children(userid, firstName, lastName, birthday) VALUES($1, $2, $3, $4)'
+    //     result = await pool.query(queryText, [parseInt(userid), firstName, lastName, birthday])
+    // }
+
+}
--- index.js
+++ index.js
@@ -1,31 +1,162 @@
-import express from 'express';
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
 
-dotenv.config()
 
+import express from 'express'
+import { get_admin_by_username, update_refresh_token, get_admin_by_refresh_token, register_user } from './controllers/database.js';
+import bodyParser from 'body-parser'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from 'cookie-parser';
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cookieParser())
+var jsonParser = bodyParser.json()
 
-const pool = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-}).promise();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser());
 
-app.get('/', async (req, res) => {
-    const [admin] = await pool.query("SELECT * FROM admin")
-    res.send(JSON.stringify(admin[0]));
-})
+
+// Serve static files from the React app
+
+// app.get('/db/users', async (request, response) => {
+//   pool.query('SELECT * FROM users', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     console.log(results)
+//     response.status(200).json(results.rows)
+//   })
+// })
+
+// app.get('/db/spouses', async (request, response) => {
+//   pool.query('SELECT * FROM spouses', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     console.log(results)
+//     response.status(200).json(results.rows)
+//   })
+// })
+
+// app.get('/db/children', async (request, response) => {
+//   pool.query('SELECT * FROM children', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     console.log(results)
+//     response.status(200).json(results.rows)
+//   })
+// })
+
+// app.get('/api/secret/', withAuth, function (req, res) {
+//   res.send('The password is potato');
+// });
+
+// app.get('/checkToken', withAuth, function (req, res) {
+//   console.log("Sending 200")
+//   res.sendStatus(200);
+// });
+
+
+app.post('/join', jsonParser, async (req, res) => {
+    // insert into users table
+    try {
+        register_user(req)
+        res.status(200).json({ msg: "Success!" });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: "There was an issue with the server. Please try again." });
+    }
+
+});
+
+
+app.post('/login', jsonParser, async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        console.log(username, password)
+        const [admin] = await get_admin_by_username(username)
+
+        if (!admin) return res.status(400).json({ msg: "Incorrect username or password" });
+
+        const match = await bcrypt.compare(password, admin.password);
+        if (!match) return res.status(400).json({ msg: "Incorrect username or password" });
+
+        const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '15s'
+        });
+        const refreshToken = jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, {
+            expiresIn: '1d'
+        });
+
+        await update_refresh_token(refreshToken, username)
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
+        });
+        res.json({ accessToken });
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({ msg: "Incorrect username or password" });
+    }
+});
+
+
+app.get('/logout', jsonParser, async (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.sendStatus(204);
+    const [admin] = await get_admin_by_refresh_token(refreshToken);
+    if (!admin) return res.sendStatus(204);
+    const username = admin.username;
+    await update_refresh_token(null, username)
+
+    res.clearCookie('refreshToken');
+    return res.sendStatus(200);
+});
+
+app.get('/token', jsonParser, async (req, res) => {
+    try {
+        console.log(req.cookies)
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) return res.sendStatus(401);
+        const [admin] = await get_admin_by_refresh_token(refreshToken)
+        if (!admin) return res.sendStatus(403);
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            const username = admin.username;
+            const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '15s'
+            });
+            res.json({ accessToken });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+app.post('/register/', function requestHandler(req, res) {
+    insertDb(req)
+    res.status(201).send(`User added`)
+});
 
 app.get("/json", (req, res) => {
     res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
 })
 
-const port = process.env.PORT || 3000;
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
